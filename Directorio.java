d524310857591d6e078ce457dc2fbde683766ce1
Alejandro <alexzgz12@hotmail.com>
2015-05-25 15:07:10 +0200

    Fuentes

---
+++ Directorio.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+
+
+public class Directorio extends Cluster {
+
+	LinkedList<EntradaDir> listaContenido;
+	
+	
+	public Directorio(){
+	
+		listaContenido = new LinkedList<EntradaDir>();
+		
+	}
+	
+	
+	public String toString(){
+	
+		return ("Dir"+listaContenido);
+		
+	}
+	
+}
