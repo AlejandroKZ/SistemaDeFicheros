6359c19f9fdddcfb7dc31c02e9b08037209c7367
Daniel Coloma <dcoloma@tid.es>
2015-05-29 08:43:30 +0200

    Code Style cleaned and first test added

--- Directorio.java
+++ Directorio.java
@@ -1,22 +1,13 @@
 import java.util.LinkedList;
 
-
 public class Directorio extends Cluster {
-
-	LinkedList<EntradaDir> listaContenido;
-	
-	
-	public Directorio(){
-	
-		listaContenido = new LinkedList<EntradaDir>();
-		
-	}
-	
-	
-	public String toString(){
-	
-		return ("Dir"+listaContenido);
-		
-	}
-	
+  LinkedList<EntradaDir> listaContenido;
+  
+  public Directorio() {
+    listaContenido = new LinkedList<EntradaDir>();
+  }
+  
+  public String toString(){
+    return ("Dir"+listaContenido);
+  }
 }
--- main.java
+++ main.java
@@ -1,13 +1,11 @@
-
 public class main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		SistemaDeFicheros ns = new SistemaDeFicheros();//Crear sistema operativo y  de ahi crear la consola dentro
-		Consola console = new Consola(ns);
-		
-		console.mostrarOpciones();
-	}
-
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    
+    SistemaDeFicheros ns = new SistemaDeFicheros();//Crear sistema operativo y  de ahi crear la consola dentro
+    Consola console = new Consola(ns);
+    
+    console.mostrarOpciones();
+  }
 }
